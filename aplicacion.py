3ea9a714913180faf03452a13bba34de1b3d9f33
Carlos Ruiz <1923010311@untels.edu.pe>
2022-05-30 19:50:39 -0500

    metodo menu listo

---
+++ aplicacion.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 19:36:53 2022
+
+@author: CARLOS
+"""
+import gestion_archivos
+
+def menu():
+    print("*****MENU PRINCIPAL*****")
+    print("1. Crear archivo")
+    print("2. Eliminar archivo")
+    print("3. Agregar contenido")
+    print("4. Mostrar contenido del archivo")
+    print("5. Salir")
+
