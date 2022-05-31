0b6af6b1288b76d47751d1ca4497804a56028d54
Carlos Ruiz <1923010311@untels.edu.pe>
2022-05-30 19:21:29 -0500

    metodo eliminar_archivo listo

--- gestion_archivo.py
+++ gestion_archivo.py
@@ -8,3 +8,7 @@ def crear_archivo(nombre, contenido):
     archivo = open(nombre,"wt")
     archivo.write(contenido)
     archivo.close()
+
+def eliminar_archivo(nombre):
+    os.remove(nombre)
+    
